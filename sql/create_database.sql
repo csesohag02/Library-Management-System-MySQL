0634214c7e23c13beca3c6efc67a9470a8491ed5
SOHAG CHAKRABORTY <94746700+csesohag02@users.noreply.github.com>
2024-12-19 20:38:17 +0530

    Update create_database.sql

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,5 +1,13 @@
+-- SQL Script: Library Management System Database Initialization
+-- This script creates the `library` database and switches to it for further operations.
+-- Author: @csesohag02
+-- GitHub: https://github.com/csesohag02
+
 -- create database
 CREATE DATABASE library;
 
 -- use the database
 USE library;
+
+-- Created and maintained by @csesohag02
+-- GitHub: https://github.com/csesohag02
